c0ca71929e40eb8d71d3049b3edb2f15b31ec8d8
roman <smtn.roman@gmail.com>
2022-09-06 12:56:56 +0200

    user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :email, presence: true
+  validates :nickname, :email, uniqueness: true
+  validates :nickname,  length: { in: 3..10 }
 end
