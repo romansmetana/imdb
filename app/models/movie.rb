ba491e7710bb9377d30a33237739f46a9c170a5a
roman <smtn.roman@gmail.com>
2022-09-07 12:32:01 +0200

    Created Rating model, movie has many ratigns

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Movie < ApplicationRecord
+  has_many :ratings
   enum :category,
        { 'No category': 0, Action: 1, Comedy: 2, Drama: 3, Fantasy: 4, Horror: 5, Mystery: 6, Romance: 7, Thriller: 8,
          Westerm: 9 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_07_100829) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_07_102932) do
   create_table "movies", force: :cascade do |t|
     t.string "title", default: "", null: false
     t.text "description", default: "", null: false
     t.integer "category", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer "rating"
+    t.integer "movie_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movie_id"], name: "index_ratings_on_movie_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +40,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_07_100829) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "ratings", "movies"
 end
